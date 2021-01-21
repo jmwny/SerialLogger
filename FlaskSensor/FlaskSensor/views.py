7d05341b03b83ee3f0f740b6ba58c147dd2c9e4d
jmwny <jmwny@users.noreply.github.com>
2021-01-21 18:14:31 -0500

    cosmetic changes

--- FlaskSensor/FlaskSensor/views.py
+++ FlaskSensor/FlaskSensor/views.py
@@ -35,6 +35,38 @@ def home():
     """Renders the home page."""
     db_vals = query_db('SELECT * FROM env ORDER BY id DESC LIMIT 1')
 
+    ### temps, one hour
+    temps = query_db('SELECT tempf FROM env ORDER BY id DESC LIMIT 720')
+    tempf = []
+    for t in temps:
+        tempf.append(float(t[0]))
+    tlow = min(tempf)
+    thigh = max(tempf)
+
+    ### humidity, one hour
+    humidity = query_db('SELECT humidity FROM env ORDER BY id DESC LIMIT 720')
+    humidityf = []
+    for f in humidity:
+        humidityf.append(float(f[0]))
+    hlow = min(humidityf)
+    hhigh = max(humidityf)
+
+    ### temps, six hours
+    temps = query_db('SELECT tempf FROM env ORDER BY id DESC LIMIT 4320')
+    tempf = []
+    for t in temps:
+        tempf.append(float(t[0]))
+    tlow2 = min(tempf)
+    thigh2 = max(tempf)
+
+    ### humidity, six hours
+    humidity = query_db('SELECT humidity FROM env ORDER BY id DESC LIMIT 4320')
+    humidityf = []
+    for f in humidity:
+        humidityf.append(float(f[0]))
+    hlow2 = min(humidityf)
+    hhigh2 = max(humidityf)
+
     return render_template(
         'index.html',
         title='Home Page',
@@ -44,6 +76,14 @@ def home():
         humidity=db_vals[0][4],
         eco2=db_vals[0][5],
         tvoc=db_vals[0][6],
+        tlow = tlow,
+        thigh = thigh,
+        hlow = hlow,
+        hhigh = hhigh,
+        tlow2 = tlow2,
+        thigh2 = thigh2,
+        hlow2 = hlow2,
+        hhigh2 = hhigh2,
         time=datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
-        year=datetime.now().year,
+        year=datetime.now().year
     )
